fe3d9410fd14771c8456c7f67e9920c520800925
Ethan Crook <ecrook@unc.edu>
2024-05-20 01:49:09 -0400

    Implement basic text-to-speech

--- src/interaction/main.py
+++ src/interaction/main.py
@@ -1,19 +1,28 @@
 import speech_recognition as sr
+import pyttsx3
 from ..private.key import OPENAI_API_KEY
+from openai import OpenAI
 
 def main(): 
-    # obtain audio from the microphone
-    r = sr.Recognizer()
+
+    recognizer = sr.Recognizer()
+    recognizer.dynamic_energy_threshold = True #adjust for ambient noise in environment
+    engine = pyttsx3.init()
+
+    # use default microphone as audio source
     with sr.Microphone() as source:
         print("Say something!")
-        audio = r.listen(source)
+        audio = recognizer.listen(source)
 
     print("Listening Complete!")
 
     try:
-        print(f"Whisper API thinks you said \"{r.recognize_whisper_api(audio, api_key=OPENAI_API_KEY)}\"")
-    except sr.RequestError as e:
-        print(f"Could not request results from Whisper API; {e}")
+        transcription = recognizer.recognize_whisper_api(audio, api_key=OPENAI_API_KEY)
+        print(transcription)
+        engine.say(transcription)
+        engine.runAndWait()
+    except sr.RequestError as error:
+        print(f"Could not request results from Whisper API; {error}")
 
 if __name__ == "__main__":
     main()
